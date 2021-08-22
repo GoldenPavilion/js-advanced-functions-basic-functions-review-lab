594653f9bee5dedaf2b0792270d33384d768b0be
GoldenPavilion <rcorystine@gmail.com>
2021-08-22 18:42:14 -0400

    completed Calculator object

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function wrapAdjective(string = "*") {
       return `You are ${string}${param}${string}!`;
     }
 }
+
+const Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    }, 
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+}
